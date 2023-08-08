071620c6d0eec6f905e37216b7f7ad427279727f
fdundar95 <79167732+fdundar95@users.noreply.github.com>
2023-08-08 12:22:11 +0300

    nc

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -2,14 +2,29 @@ import { useFilterContext } from '../context/filter_context';
 import { BsFillGridFill, BsList } from 'react-icons/bs';
 import styled from 'styled-components';
 const Sort = () => {
-  const { filtered_products: products, grid_view } = useFilterContext();
+  const {
+    filtered_products: products,
+    grid_view,
+    setGridView,
+    setListView,
+    sort,
+    updateSort,
+  } = useFilterContext();
   return (
     <Wrapper>
       <div className='btn-container'>
-        <button type='button' className={grid_view ? 'active' : null}>
+        <button
+          type='button'
+          className={grid_view ? 'active' : null}
+          onClick={setGridView}
+        >
           <BsFillGridFill />
         </button>
-        <button type='button' className={!grid_view ? 'active' : null}>
+        <button
+          type='button'
+          className={!grid_view ? 'active' : null}
+          onClick={setListView}
+        >
           <BsList />
         </button>
       </div>
@@ -17,7 +32,13 @@ const Sort = () => {
       <hr />
       <form>
         <label htmlFor='sort'>sort by </label>
-        <select name='sort' id='sort' className='sort-input'>
+        <select
+          name='sort'
+          id='sort'
+          className='sort-input'
+          value={sort}
+          onChange={updateSort}
+        >
           <option value='price-lowest'>price (lowest)</option>
           <option value='price-highest'>price (highest)</option>
           <option value='name-a'>price (a-z)</option>
